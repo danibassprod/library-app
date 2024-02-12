3f9b5b57352f25fab1c1f880d082a504f57ca330
Daniel <cristianobajista21@gmail.com>
2024-02-12 10:16:06 -0400

    Add a function that displays  array elements

--- js-file.js
+++ js-file.js
@@ -16,4 +16,58 @@ function AddBookToLibrary(title, author, pages, read) {
 
 AddBookToLibrary('Biblie', 'God', 787, 'Not yet read')
 AddBookToLibrary('Escape', 'John Doe', 321, 'Read')
-console.log(myLibrary)
+
+function displayLibrary() {
+    myLibrary.forEach(function(book) {
+
+        const contentArea = document.querySelector('.content');
+        const bookContainer = document.createElement('div');
+        bookContainer.classList.add('book');
+    
+        const titleIcon = document.createElement('img')
+        titleIcon.classList.add('logo');
+        titleIcon.src = 'img/title.svg';
+        titleIcon.alt = 'title icon';
+        bookContainer.appendChild(titleIcon);
+
+        const title = document.createElement('h3');
+        title.textContent = book.title;
+        title.classList.add('title', 'text');
+        bookContainer.appendChild(title);
+
+        const authorIcon = document.createElement('img');
+        authorIcon.classList.add('logo');
+        authorIcon.src = 'img/author.svg';
+        authorIcon.alt = 'author icon';
+        bookContainer.appendChild(authorIcon);
+        
+        const author = document.createElement('p');
+        author.textContent = book.author;
+        author.classList.add('author', 'text');
+        bookContainer.appendChild(author);
+
+        const pagesIcon = document.createElement('img');
+        pagesIcon.classList.add('logo');
+        pagesIcon.src = 'img/year.svg';
+        pagesIcon.alt = 'pages icon';
+        bookContainer.appendChild(pagesIcon);
+        
+        const pages = document.createElement('p');
+        pages.textContent = book.pages;
+        pages.classList.add('pages', 'text');
+        bookContainer.appendChild(pages);
+        
+        const readIcon = document.createElement('img');
+        readIcon.classList.add('logo');
+        readIcon.src = 'img/read.svg';
+        readIcon.alt = 'read icon';
+        bookContainer.appendChild(readIcon);
+
+        const readState = document.createElement('p');
+        readState.textContent = book.read;
+        readState.classList.add('read', 'text');
+        bookContainer.appendChild(readState);
+
+        contentArea.appendChild(bookContainer);
+    })
+}
